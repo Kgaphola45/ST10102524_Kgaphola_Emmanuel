6e250e2074874b268c09e41fc7b55a1208f25a96
Emmanuel <It45ma@protonmail.ch>
2023-04-26 00:25:14 +0200

    Added new external class to write my funcions.

---
+++ ST10102524_Kgaphola_Emmanuel/RecipeFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10102524_Kgaphola_Emmanuel
+{
+    internal class RecipeFunctions
+    {
+    }
+}
