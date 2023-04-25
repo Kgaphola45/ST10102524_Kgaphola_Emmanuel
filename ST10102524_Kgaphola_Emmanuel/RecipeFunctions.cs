a165df20b8baf514cafeca3df3b8f8e169abfd52
Emmanuel <It45ma@protonmail.ch>
2023-04-26 00:36:43 +0200

    Private method to enter ingredients for the recipe.

--- ST10102524_Kgaphola_Emmanuel/RecipeFunctions.cs
+++ ST10102524_Kgaphola_Emmanuel/RecipeFunctions.cs
@@ -38,6 +38,53 @@ namespace ST10102524_Kgaphola_Emmanuel
         private List<string> steps;
 
 
+        public Recipe()
+        {
+            ingredients = new List<Ingredient>();
+            steps = new List<string>();
+        }
+
+        public void EnterRecipeDetails()
+        {
+            EnterIngredients();
+            EnterSteps();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nRecipe details entered successfully!");
+            Console.ResetColor();
+        }
+
+        
+        private void EnterIngredients()
+        {
+            Console.Write("How many ingredients are in your recipe: ");
+            if (int.TryParse(Console.ReadLine(), out int numIngredients))
+            {
+                for (int i = 0; i < numIngredients; i++)
+                {
+                    Console.Write($"Enter the name of ingredient number {i + 1}: ");
+                    string name = Console.ReadLine();
+                    Console.Write($"Enter the quantity of ingredient number {i + 1}: ");
+                    double quantity = double.Parse(Console.ReadLine());
+                    Console.Write($"Enter the unit of measurement for ingredient number {i + 1}: ");
+                    string unit = Console.ReadLine();
+
+                    ingredients.Add(new Ingredient(name, quantity, unit));
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"\nIngredient number {i + 1} added successfully.\n");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid input. Please enter a valid number.\n");
+                Console.ResetColor();
+            }
+        }
+
+
+
     }
 
 
