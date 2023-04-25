48b33a0fec7328e81da00a467d97586803a37893
Emmanuel <It45ma@protonmail.ch>
2023-04-26 00:32:18 +0200

    Properties for the ingredient's name, quantity, and unit of measurement. And Constructor to initialize an Ingredient object.

--- ST10102524_Kgaphola_Emmanuel/RecipeFunctions.cs
+++ ST10102524_Kgaphola_Emmanuel/RecipeFunctions.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ST10102524_Kgaphola_Emmanuel
 {
-    internal class RecipeFunctions
+    class Ingredient
     {
+
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+
+        private double originalQuantity;
+
+        public Ingredient(string name, double quantity, string unit)
+        {
+            Name = name;
+            Quantity = quantity;
+            Unit = unit;
+            originalQuantity = quantity;
+        }
+
+
+
+
     }
+
+
 }
